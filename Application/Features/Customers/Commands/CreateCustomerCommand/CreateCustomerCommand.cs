b506881569beae8eaaa8df69238432168169c68d
mario_alex <marioatoroh@gmail.com>
2024-02-28 12:27:37 -0300

    Agegando la entidad de dominio

---
+++ Application/Features/Customers/Commands/CreateCustomerCommand/CreateCustomerCommand.cs
@@ -0,0 +1,16 @@
+﻿using Application.Wrappers;
+using MediatR;
+using System;
+
+namespace Application.Features.Customers.Commands.CreateCustomerCommand
+{
+    public class CreateCustomerCommand : IRequest<Response<int>>
+    {
+        public string Nombe { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+    }
+}
---
+++ Domain/Entities/Customer.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Common;
+using System;
+
+namespace Domain.Entities
+{
+    public class Customer : AuditableBaseEntity
+    {
+        private int _edad;
+
+        public string Nombe { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+
+        public int Edad
+        {
+            get
+            {
+                if(this._edad <= 0)
+                {
+                    this._edad = new DateTime(DateTime.Now.Subtract(this.FechaNacimiento).Ticks).Year - 1;
+                }
+                return this._edad;
+            }
+        }
+    }
+}
